03788529ce01d22bc242b9fe33a4fde41faa3060
DWS-paris <j.noyer@digitalworkshop.fr>
2019-10-21 16:58:59 +0200

    TP js

---
+++ 21oct/exo6/js/script.js
@@ -0,0 +1,19 @@
+/* 
+Attendre le chargement du DOM
+*/
+document.addEventListener('DOMContentLoaded', () => {
+
+    /* 
+    - Demander à l'utilisateur de choisir une note
+    - Stocker dans un tableau les notes ajoutées par l'utilisateur
+    - Calculer la moyenne des notes
+        - faire une boucle pour additionner toutes les notes
+        - diviser le total des notes par le nombre de notes
+    */
+
+
+
+
+
+
+}); // Fin de la fonction de chargement du DOM
