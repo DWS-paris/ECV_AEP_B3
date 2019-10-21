68094143d09cf1cdbbf1c8055926ddafedfd08ee
DWS-paris <j.noyer@digitalworkshop.fr>
2019-10-21 18:31:12 +0200

    Fin de journée

--- 21oct/exo6/js/script.js
+++ 21oct/exo6/js/script.js
@@ -15,6 +15,7 @@ document.addEventListener('DOMContentLoaded', () => {
     Déclarations
     */
         // Variables du DOM
+        let subTitle = document.querySelector('h2');
         let addNote = document.querySelector('#addNote');
         let displayAverage = document.querySelector('.displayAverage');
         let noteList = document.querySelector('#noteList');
@@ -26,10 +27,36 @@ document.addEventListener('DOMContentLoaded', () => {
     //
 
     /* 
+    Fonctions
+    */
+        let setAverage = () => {
+            // Mettre à 0 le total de notes
+            total = 0;
+            
+            // Boucle sur la tableau notes
+            for( let item of notes ){
+                total += item;
+            };
+
+            // Calculer la moyenne
+            subTitle.innerHTML += ' <em>' + total / notes.length + ' sur 20</em>';
+
+            // Ajouter la class open sur la balise subTitle
+            subTitle.classList.add('open');
+        };
+    //
+
+    /* 
     Lancer l'IHM
     */
         // Capter le clic sur le bouton addNote pour ajouter une note
         addNote.addEventListener( 'click', () => {
+            // Retirer la class open sur la balise subTitle
+            subTitle.classList.remove('open');
+
+            // Changer le contenu de la balise
+            subTitle.innerHTML = 'La moyennne est de :';
+
             // Demander à l'utilisateur d'ajouter une note sur 20
             let newNote = prompt('Ajouter une note sur 20');
             console.log( +newNote );
@@ -38,8 +65,8 @@ document.addEventListener('DOMContentLoaded', () => {
             // Convertir un texte en entier (ex. : '12' devient 12)
             let convertedNote = +newNote;
 
-            // Verifierr la valeur
-            if( isNaN(convertedNote) ){ // La valeur n'est pas un nombre (Note A Number)
+            // Verifier la valeur
+            if( isNaN(convertedNote) ){ // La valeur n'est pas un nombre (NaN : Note a Number)
                 alert('Ajouter un entier !!');
             }
             else if( convertedNote > 20 ){ // La valeur est au-dessus de 20
@@ -48,8 +75,22 @@ document.addEventListener('DOMContentLoaded', () => {
             else{
                 // La valeur est bonne, ajouter la dans un tableau
                 notes.push( convertedNote );
-                console.log( notes );
+
+                // Ajouter la note dans la liste
+                noteList.innerHTML += '<li>' + convertedNote + ' sur 20</li>';
+
+                // Vérifier s'il y a au moins deux notes dans la tableau
+                if( notes.length >= 2 ){
+                    // Ajouter la class open sur la balise displayAverage
+                    displayAverage.classList.add('open');
+                };
             }
+        });
+
+        // Capter le clic sur la balise .displayAverage
+        displayAverage.addEventListener( 'click', () => {
+            // Appeler la fonction pour calculer la moyenne
+            setAverage();
         });
     //
 
