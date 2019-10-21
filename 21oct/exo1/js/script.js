f5256df4c715c6d35cd5d05fa21406665cd59a7d
DWS-paris <j.noyer@digitalworkshop.fr>
2019-10-21 14:40:56 +0200

    JS

---
+++ 21oct/exo1/js/script.js
@@ -0,0 +1,18 @@
+/* 
+Attendre le chargement du DOM
+*/
+document.addEventListener('DOMContentLoaded', () => {
+
+    
+    /* 
+    Déclaration de variables
+    */
+        let 
+    //
+
+
+
+
+
+
+}); // Fin de la fonction de chargement du DOM
---
+++ _base/js/script.js
@@ -0,0 +1,8 @@
+/* 
+Attendre le chargement du DOM
+*/
+document.addEventListener('DOMContentLoaded', () => {
+
+    console.log('DOM chargé');
+
+}); // Fin de la fonction de chargement du DOM
